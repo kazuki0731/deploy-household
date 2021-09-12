edc2df57f72450922416b630c081b777741d3d80
kawabata <kazuki.kawabata02@gmail.com>
2021-09-13 03:49:39 +0900

    9/12

--- server/db/pool.js
+++ server/db/pool.js
@@ -13,12 +13,4 @@ const pool = new Pool({
   },
 });
 
-// const pool = new Pool({
-//   database: "household",
-//   user: "kawabata",
-//   host: "localhost",
-//   password: "XLwdAX2m",
-//   port: 5432,
-// });
-
 module.exports = pool;
